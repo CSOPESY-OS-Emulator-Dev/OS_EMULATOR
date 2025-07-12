6187a65c2e22b94882707a82fd45f8771646dd60
0niiSean <seanandreiolores@gmail.com>
2025-07-12 14:59:56 +0800

    .h, .cpp, and coconut

---
+++ OS_EMULATOR/OS_Memory/source/MemoryManager.cpp
@@ -0,0 +1,40 @@
+#include "../header/MemoryManager.h"
+#include "../header/FFMemAllocator.h"
+
+MemoryManager* MemoryManager::instance = nullptr;
+
+MemoryManager::MemoryManager() {
+    // Constructor is private or protected in the header file
+}
+
+MemoryManager* MemoryManager::getInstance() {
+    if (instance == nullptr) {
+        instance = new MemoryManager();
+    }
+    return instance;
+}
+
+void MemoryManager::initialize(size_t maxMemorySize) {
+    this->allocator = std::make_unique<FFMemAllocator>(maxMemorySize);
+}
+
+void* MemoryManager::allocate(Process& process) {
+    return this->allocator->allocate(process);
+}
+
+void MemoryManager::deallocate(Process& process) {
+    this->allocator->deallocate(process);
+}
+
+void MemoryManager::createSnapshot(int quantumCycle) {
+    if (allocator) {
+        allocator->createSnapshot(quantumCycle);
+    } else {
+        std::cerr << "[MemoryManager] Cannot create snapshot: Allocator not initialized.\n";
+    }
+    
+}
+
+std::string MemoryManager::visualizeMemory() const {
+    return this->allocator->visualizeMemory();
+}
--- OS_EMULATOR/source/RRScheduler.cpp
+++ OS_EMULATOR/source/RRScheduler.cpp
@@ -10,17 +10,40 @@ RRScheduler::~RRScheduler() {
 
 void RRScheduler::execute() {
     std::lock_guard<std::mutex> lock(queueMutex); // Add this lock
+
     if (!this->ReadyQueue.empty()) {
         // Loop through all CPU cores
         for (const auto& core : GlobalScheduler::getInstance()->cores) {
             // If there are no more processes to schedule, stop
             if (this->ReadyQueue.empty()) break;
+
             // If the core is free, assign a process
             if (!core->isOccupied()) {
                 // Get the process at the front of the queue
                 auto process = this->ReadyQueue.front();
+                // Process* process = this->ReadyQueue.front();
                 this->ReadyQueue.erase(this->ReadyQueue.begin()); // Remove it from the queue
-                core->assignProcess(process, this->quantumCycles); // Assign the process to the core with quantumCycles ticks
+
+                // core->assignProcess(process, this->quantumCycles); // Assign the process to the core with quantumCycles ticks
+
+                // Attempt to allocate memory for the process
+                void* memAddress = MemoryManager->allocate(*process);
+
+                if (memAddress == nullptr) {
+                    // Not enough memory: requeue process
+                    std::cout << "[RRScheduler] Not enough memory for Process "
+                              << process->getProcessID() << ". Requeued.\n";
+                    this->ReadyQueue.push_back(process);
+                } else {
+                    // Memory allocation successful
+                    process->setBaseAddress(memAddress);
+
+                    std::cout << "[RRScheduler] Allocated memory and scheduled Process "
+                              << process->getProcessID() << " on a core.\n";
+
+                    core->assignProcess(process, this->quantumCycles);
+                }
+                
                 // If the process is not finished, add it back to the end of the queue later
                 // (This is handled inside CoreThread automatically by checking ticks and re-queuing)
             }
